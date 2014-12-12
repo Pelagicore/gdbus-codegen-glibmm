8d7420ea8116a9f4e1c41df2d464ee1242fc1bca
Jonatan Pålsson <jonatan.palsson@pelagicore.com>
2014-12-12 11:47:35 +0100

    Support running tests with space in path

--- test/runtests.sh
+++ test/runtests.sh
@@ -5,7 +5,7 @@ DIR=$(cd $(dirname "$0"); pwd)
 cd stub
 mkdir -p build
 cd build
-cd .. ; rm -rf build ; mkdir build; cd build; cmake .. -DCODEGEN=$DIR/../gdbus-codegen-glibmm.py -DCMAKE_BUILD_TYPE=Debug
+cd .. ; rm -rf build ; mkdir build; cd build; cmake .. -DCODEGEN="$DIR/../gdbus-codegen-glibmm.py" -DCMAKE_BUILD_TYPE=Debug
 make
 ./stubtest &
 STUB_PID=$!
@@ -13,7 +13,7 @@ STUB_PID=$!
 cd ../../proxy
 mkdir -p build
 cd build
-cd .. ; rm -rf build ; mkdir build; cd build; cmake .. -DCODEGEN=$DIR/../gdbus-codegen-glibmm.py -DCMAKE_BUILD_TYPE=Debug
+cd .. ; rm -rf build ; mkdir build; cd build; cmake .. -DCODEGEN="$DIR/../gdbus-codegen-glibmm.py" -DCMAKE_BUILD_TYPE=Debug
 
 make
 ./proxytest&
