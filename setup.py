3780bcef93c4b9271cd40ce1e5b56201b2d1b7d6
Jonatan Pålsson <jpalsson@luxoft.com>
2018-05-30 13:31:26 +0200

    codegen: Add --version
    With this commit, we also move to specifying the version of the code
    generator in codegen_glibmm/__init__.py rather than setup.py. This makes
    it easier for both the code generator itself and the setup code to know
    of the version.

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
+import codegen_glibmm
+
 setup(
     name = "gdbus-codegen.glibmm",
-    version = "0.1",
+    version = codegen_glibmm.version,
     packages = find_packages(),
     entry_points = {
         'console_scripts': [
