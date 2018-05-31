a86406fd1e8f03427648a353697d40d907697ff8
Jonatan Pålsson <jpalsson@luxoft.com>
2018-05-31 13:34:54 +0200

    build: Install template files in system

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ import codegen_glibmm
 setup(
     name = "gdbus-codegen.glibmm",
     version = codegen_glibmm.version,
+    include_package_data=True,
     packages = find_packages(),
+    package_data = {
+        "": ["*.templ"]
+    },
     entry_points = {
         'console_scripts': [
             'gdbus-codegen-glibmm = codegen_glibmm.codegen_main:codegen_main',
