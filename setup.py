9eb3a679fa4493ac8bd053962af9cd7aedbe4036
Jonatan Pålsson <jonatan.palsson@pelagicore.com>
2018-06-01 16:15:09 +0200

    build: Install CMake Config files
    This helps CMake find the code generator and decide its version

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
+import sys, shutil, os
 import codegen_glibmm
+
+cmake_config_file = "cmake-files/gdbus-codegen-glibmm-config.cmake"
+cmake_version_file = "cmake-files/gdbus-codegen-glibmm-config-version.cmake"
+
+shutil.copyfile(cmake_version_file + ".in", cmake_version_file)
+os.system("sed -i 's/%VERSION%/"+codegen_glibmm.version+"/g' " + cmake_version_file)
 
 setup(
     name = "gdbus-codegen.glibmm",
@@ -15,5 +22,7 @@ setup(
         'console_scripts': [
             'gdbus-codegen-glibmm = codegen_glibmm.codegen_main:codegen_main',
         ]
-    }
+    },
+    data_files = [(sys.prefix + "/lib/cmake/gdbus-codegen-glibmm-" + codegen_glibmm.version,
+                   [cmake_version_file, cmake_config_file])]
  )
